c15b96ffc7855200d4b056b81cab56116b0939c3
bluemangoo <chenfy2006@qq.com>
2023-05-06 21:01:01 +0800

    fix: logger currentInfo

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,64 +4,69 @@ import * as logSymbols from "log-symbols";
 class Logger {
     private removeAndNew: boolean = false;
     private latest: string = "";
-    private readonly cls = "\u001B[1F\u001B[2K\u001B[1G";
+    private readonly cls = "\u001B[F\u001B[2K";
+    private readonly stream: NodeJS.WriteStream = process.stdout;
+
+    private clearLine() {
+        this.stream.write(this.cls);
+    }
+
+    private writeLine(message?: any, ...optionalParams: any[]) {
+        this.stream.write(Buffer.from([message, ...optionalParams].join(" ") + "\n"));
+    }
 
     public info(msg: string) {
-        let prefix = "";
         if (this.removeAndNew) {
-            prefix = this.cls;
+            this.clearLine();
         }
-        console.log(prefix, logSymbols.info, chalk.cyan(msg));
+        console.log(logSymbols.info, chalk.cyan(msg));
         if (this.removeAndNew) {
-            console.log(logSymbols.info, chalk.cyan(this.latest));
+            this.reWriteCurrent();
         }
     }
 
     currentInfo(msg: string) {
         this.latest = msg;
-        let prefix = "";
         if (this.removeAndNew) {
-            prefix = this.cls;
+            this.clearLine();
         }
-        console.log(prefix, "✨", msg);
+        this.removeAndNew = true;
+        this.writeLine("✨", msg);
+    }
 
+    private reWriteCurrent() {
+        this.writeLine("✨", this.latest);
     }
 
     cancelCurrent() {
         if (this.removeAndNew) {
-            console.log(this.cls);
+            this.clearLine();
             this.removeAndNew = false;
         }
     }
 
     public warn(msg: string) {
-        let prefix = "";
         if (this.removeAndNew) {
-            prefix = this.cls;
+            this.clearLine();
         }
-        console.warn(prefix, logSymbols.warning, chalk.yellow(msg));
+        console.warn(logSymbols.warning, chalk.yellow(msg));
         if (this.removeAndNew) {
-            this.info(this.latest);
+            this.reWriteCurrent();
         }
     }
 
     public throwOrWarn(ignoreError: boolean, err: unknown, further?: string) {
         if (ignoreError) {
-            let prefix = "";
-
             if (this.removeAndNew) {
-                prefix = this.cls;
+                this.clearLine();
             }
-
             if (further) {
-                console.warn(prefix, logSymbols.warning, chalk.yellow(further));
-                console.warn(err);
-            } else {
-                console.warn(prefix, err);
+                console.warn(logSymbols.warning, chalk.yellow(further));
             }
+            console.warn(err);
 
             if (this.removeAndNew) {
-                this.info(this.latest);
+                this.reWriteCurrent();
             }
         } else {
             this.error(err, further);
@@ -76,13 +81,12 @@ class Logger {
     }
 
     public success(msg: string) {
-        let prefix = "";
         if (this.removeAndNew) {
-            prefix = this.cls;
+            this.clearLine();
         }
-        console.log(prefix, logSymbols.success, chalk.green(msg));
+        this.writeLine(logSymbols.success, chalk.green(msg));
         if (this.removeAndNew) {
-            this.info(this.latest);
+            this.reWriteCurrent();
         }
     }
 }
